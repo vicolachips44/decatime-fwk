bc5889dcdbf544aadbe737dd5539f8a79f99de3e
Vicolachips44 <vga.nantes@gmail.com>
2013-09-21 08:12:00 +0200

    Label working

---
+++ src/org/decatime/ui/BaseBitmapElement.hx
@@ -0,0 +1,84 @@
+package org.decatime.ui;
+
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.PixelSnapping;
+import flash.geom.Rectangle;
+
+import org.decatime.ui.layout.ILayoutElement;
+
+class BaseBitmapElement extends Bitmap implements ILayoutElement {
+	private var sizeInfo:Rectangle;
+	private var hgap:Int;
+	private var vgap:Int;
+
+	/**
+	* Default constructor.
+	* @param name The name for this BaseShapeElement instance	
+	*/
+	public function new(bitmapData:BitmapData, ?pixelSnapping:PixelSnapping, smoothing:Bool = false) {
+		super(bitmapData, pixelSnapping, smoothing);
+		hgap = 0;
+		vgap = 0;
+	}
+
+	public function setHorizontalGap(value:Float): Void {
+		this.hgap = value;
+	}
+
+	public function getHorizontalGap(): Float {
+		return this.hgap;
+	}
+
+	public function setVerticalGap(value:Float): Void {
+		this.vgap = value;
+	}
+
+	public function getVerticalGap(): Float {
+		return this.vgap;
+	}
+
+	/**
+	* ILayoutElement implementation. will make the position X and Y match
+	* the provided Rectangle instance x and y.
+	*
+	* @see org.decatime.display.ui.ILayoutElement
+	*
+	* @throws nme.error.Error if the provided Rectangle argument is null
+	*/
+	public function refresh(r:Rectangle): Void {
+		if (r == null) {
+			throw new Error("provided Rectangle instance value is null");
+		}
+		this.sizeInfo = r;
+		
+		x = r.x + this.hgap;
+		y = r.y + this.vgap;
+
+		// With and height depends on what was the source
+		// in the BitmapData element.
+	}
+
+	/**
+	* ILayoutElement implementation. returns the sizeInfo property that was
+	* setted in the refresh method call.
+	*
+	* @return nme.geom.Rectangle containing the dimension.
+	*/
+	public function getCurrSize(): Rectangle {
+		return sizeInfo;
+	}
+
+	/**
+	* ILayoutElement implementation. 
+	* Toggle the visibility of this Shape depending on the <code>value</code> 
+	* param
+	*
+	* @param value a Boolean value to toggle the visibility
+	*
+	* @return Void
+	*/
+	public function setVisible(value:Bool): Void {
+		this.visible = value;
+	}
+}
---
+++ src/org/decatime/ui/component/Label.hx
@@ -0,0 +1,151 @@
+package org.decatime.ui.component;
+
+import flash.display.BitmapData;
+import flash.text.TextField;
+import flash.text.TextFormat;
+import flash.text.TextFieldType;
+import flash.text.TextFieldAutoSize;
+import flash.text.TextFormatAlign;
+import flash.geom.Rectangle;
+import flash.display.PixelSnapping;
+
+import org.decatime.ui.BaseBitmapElement;
+
+class Label extends BaseBitmapElement {
+	private var text:String;
+	private var color:Int;
+	private var align:String;
+	private var fontName:String;
+	private var fontSize:Int;
+
+	private var tfield:TextField;
+
+
+	private var bmData:BitmapData;
+
+	public function new(text:String, color:Int = 0x000000, ?align:String = 'left') {
+		bmData = new BitmapData();
+		super(bmData, PixelSnapping.NEVER);
+
+		this.text = text;
+		this.color = color;
+		this.align = align;
+
+		tfield = new TextField();
+		tfield.selectable = false;
+		tfield.autoSize = TextFieldAutoSize.LEFT;
+		tfield.mouseEnabled = false;
+		tfield.text = label;
+
+		this.fontName = "";
+		this.fontSize = 12;
+	}
+
+	public override function refresh(r:Rectangle): Void {
+		super.refresh(r);
+		this.draw();
+		this.x = r.x;
+		this.y = r.y;
+
+	}
+
+	public function setFontName(name:String): Void {
+		this.fontName = name;
+	}
+
+	public function setFontSize(size:Int): Void {
+		this.fontSize = size;
+		this.refresh(this.getCurrSize());
+	}
+
+	public function getText(): String {
+		return this.text;
+	}
+
+	public function setText(value:String): Void {
+		this.text = value;
+		this.refresh(this.getCurrSize());
+	}
+
+	public function getColor(): Int {
+		return this.color;
+	}
+
+	public function setColor(value:Int): Void {
+		this.color = value;
+		this.refresh(this.getCurrSize());
+	}
+
+	public function getAlign(): String {
+		return this.align;
+	}
+
+	public function setAlign(value:String): Void {
+		this.align = value;
+		this.refresh(this.getCurrSize());
+	}
+
+	private function draw(): Void {
+		if (this.fontName != "") {
+			createEmbeddedFontTextFormat();
+		} else {
+			createSimpleTextFormat();
+		}
+
+		var bmCache:BitmapData = new BitmapData(
+			Std.int(this.sizeInfo.width), 
+			Std.int(this.sizeInfo.height), 
+			true, 
+			0x000000
+		);
+	
+		if (this.align == 'left') {
+			bmCache.draw(
+				tfield,
+				new Matrix(1, 0, 0 , 1 , 2, (this.sizeInfo.height / 2) - (tfield.textHeight / 2)),  // add a two pixel margin
+				null, 
+				null, 
+				null, 
+				true 
+			);
+		} else if (this.align == 'center') {
+			bmCache.draw(
+				tfield,
+				new Matrix(1, 0, 0 , 1 , (this.sizeInfo.width / 2) - (tfield.textWidth / 2), (this.sizeInfo.height / 2) - (tfield.textHeight / 2)), 
+				null, 
+				null, 
+				null, 
+				true 
+			);
+		} else if (this.align== 'right') {
+			bmCache.draw(
+				tfield,
+				new Matrix(1, 0, 0 , 1 ,
+					this.sizeInfo.width - tfield.textWidth - 2, 
+					(this.sizeInfo.height / 2) - (tfield.textHeight / 2)), 
+				null, 
+				null, 
+				null, 
+				true 
+			);
+		}
+
+	}
+
+	private function createSimpleTextFormat(): Void {
+		// TODO See what we should do here...
+	}
+
+	private function createEmbeddedFontTextFormat(): Void {
+		var format:TextFormat = new TextFormat(
+			this.fontName, 
+			this.fontSize, 
+			in_color,
+			true
+		);
+
+		tfield.embedFonts = true;
+		tfield.defaultTextFormat = format;
+		tfield.setTextFormat(format);
+	}
+}
