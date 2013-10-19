f74d2632972022b142712e79ee05d399723da7d5
Vicolachips44 <vga.nantes@gmail.com>
2013-10-19 14:13:22 +0200

    no performance gain using a thread for drawing on a shape object

--- src/org/decatime/ui/component/canvas/DrawingSurface.hx
+++ src/org/decatime/ui/component/canvas/DrawingSurface.hx
@@ -6,23 +6,17 @@ import flash.geom.Rectangle;
 import flash.geom.Point;
 import flash.display.Graphics;
 
-import cpp.vm.Thread;
-
 import org.decatime.ui.component.BaseContainer;
 import org.decatime.ui.BaseShapeElement;
 
 class DrawingSurface extends BaseContainer {
-	private var drawingFeedBack: BaseShapeElement;
+	private static var drawingFeedBack: BaseShapeElement;
 
 	private var bmp : Bitmap;
-
 	private var startx: Float;
 	private var starty: Float;
 	private static var swithTo: Bool;
 	private var absRectangle: Rectangle;
-	private var fg: Graphics;
-	private var pt: Point;
-	private var t1: Thread;
 
 	public function new(name:String) {
 		super(name);
@@ -43,10 +37,12 @@ class DrawingSurface extends BaseContainer {
 
 	private function onMouseUp(e:MouseEvent): Void {
 		this.removeEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);
-		
-		this.bmp.bitmapData.draw(drawingFeedBack);
-		drawingFeedBack.graphics.clear();
 
+		this.bmp.bitmapData.lock();
+		this.bmp.bitmapData.draw(drawingFeedBack);
+		this.bmp.bitmapData.unlock();
+
+		drawingFeedBack.graphics.clear();
 	}
 
 	private function processDown(xpos:Float, ypos:Float): Void {
@@ -57,7 +53,7 @@ class DrawingSurface extends BaseContainer {
 			3, 
 			0x000000, 
 			1.0, 
-			true, // pixelHinting 
+			false, // pixelHinting 
 			flash.display.LineScaleMode.NONE, 
 			flash.display.CapsStyle.ROUND, 
 			flash.display.JointStyle.ROUND, 
@@ -66,24 +62,12 @@ class DrawingSurface extends BaseContainer {
 	}
 
 	private function processMove(xpos:Float, ypos:Float): Void {
-		var posX:Float = xpos;
-		var posY:Float = ypos;
-
-		t1.sendMessage(drawingFeedBack);
-		t1.sendMessage(new Point(posX, posY));
-	}
-
-	static function drawLine(): Void {
-		while (true) {
-			var feedback: BaseShapeElement = Thread.readMessage(true);
-			var pt:Point = Thread.readMessage(true);
-
-			if (swithTo) {
-				feedback.graphics.moveTo(pt.x, pt.y);
-				swithTo = false;
-			} else {
-				feedback.graphics.lineTo(pt.x, pt.y);
-			}
+		var pt:Point = new Point(xpos, ypos);
+		if (swithTo) {
+			drawingFeedBack.graphics.moveTo(pt.x, pt.y);
+			swithTo = false;
+		} else {
+			drawingFeedBack.graphics.lineTo(pt.x, pt.y);
 		}
 	}
 
@@ -95,11 +79,9 @@ class DrawingSurface extends BaseContainer {
 		super.initializeComponent();
 
 		drawingFeedBack = new BaseShapeElement('canvas');
-		
+
 		this.container.create(1.0, drawingFeedBack);
 		this.addChild(drawingFeedBack);
-
-		t1 = Thread.create(drawLine);
 	}
 
 	private override function initializeEvent(): Void {
