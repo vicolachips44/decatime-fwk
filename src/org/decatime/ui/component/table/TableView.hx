53685b6317888a88e81f8dc741c7893da7cdc18a
Vicolachips44 <vga.nantes@gmail.com>
2013-10-19 17:35:21 +0200

    started...

---
+++ src/org/decatime/ui/component/table/TableView.hx
@@ -0,0 +1,11 @@
+package org.decatime.ui.component.table;
+
+import org.decatime.ui.component.BaseContainer;
+
+class TableView extends BaseContainer {
+
+	
+	private override function initializeComponent(): Void {
+
+	}
+}
