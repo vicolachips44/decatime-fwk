345839687878ff20a69c3f33da30d714ffc54e0e
Vicolachips44 <vga.nantes@gmail.com>
2013-10-23 18:51:05 +0200

    selection row workin

--- src/com/demo/WxTableViewDemo.hx
+++ src/com/demo/WxTableViewDemo.hx
@@ -37,7 +37,7 @@ class WxTableViewDemo extends Window implements IObserver implements IPrintable 
 		var r:Int = 0;
 		var c: Int = 0;
 
-		for (r in 0...10000) {
+		for (r in 0...5000) {
 			for (c in 0...4) {
 				this.myTable.addCell(r, c, 'content_row' + r + "_col" + c);		
 			}
--- src/org/decatime/ui/component/table/TableView.hx
+++ src/org/decatime/ui/component/table/TableView.hx
@@ -5,6 +5,7 @@ import haxe.ds.IntMap;
 import flash.geom.Rectangle;
 import flash.display.Graphics;
 import flash.display.Bitmap;
+import flash.events.MouseEvent;
 
 import org.decatime.ui.BaseSpriteElement;
 import org.decatime.ui.component.BaseContainer;
@@ -23,6 +24,8 @@ class TableView extends BaseContainer implements IObserver {
 	public var spreadLastColumn(default, default): Bool;
 	public var rowHeight(default, default): Float;
 	public var headerHeight(default, default): Float;
+	public var selectedRowIndex(default, null): Int;
+
 	private var topRowIndex:Int;
 	private var bottomRowIndex: Int;
 	private var vsBar1: VerticalScrollBar;
@@ -43,6 +46,7 @@ class TableView extends BaseContainer implements IObserver {
 		this.rowHeight = 20;
 		this.headerHeight = 24;
 		this.topRowIndex = 0;
+		this.selectedRowIndex = -1;
 	}
 
 	public function getGridSprite(): BaseSpriteElement {
@@ -186,11 +190,26 @@ class TableView extends BaseContainer implements IObserver {
 		for (col in columns) {
 			col.draw(g);
 		}
+		if (this.selectedRowIndex > -1) {
+			this.drawSelectedRow(g);
+		}
 	}
 
 	private function drawBackground(g:Graphics, r:Rectangle): Void {
 		g.beginFill(0xffffff);
 		g.drawRect(r.x, r.y, r.width, r.height);
+		g.endFill();
+	}
+
+	private function drawSelectedRow(g:Graphics): Void {
+		var rcoord:Float = this.selectedRowIndex - this.topRowIndex;
+		var x: Float = this.gridArea.getCurrSize().x;
+		var y: Float = this.gridArea.getCurrSize().y + (rcoord * this.rowHeight) + this.headerHeight;
+		var w: Float = this.gridArea.getCurrSize().width;
+		var h: Float = this.rowHeight;
+
+		g.beginFill(0xcccccc, 0.7);
+		g.drawRect(x, y, w, h);
 		g.endFill();
 	}
 
@@ -224,8 +243,23 @@ class TableView extends BaseContainer implements IObserver {
 
 		gridSprite = new BaseSpriteElement('gridContainer');
 		gridSprite.isContainer = false;
+		gridSprite.addEventListener(MouseEvent.CLICK, onMouseEvtClick);
 		
 		this.addChild(this.vsBar1);
 		this.addChild(gridSprite);
+	}
+
+	private function onMouseEvtClick(e:MouseEvent): Void {
+		trace (e.localY);
+		var searchY: Float = this.gridArea.getCurrSize().y + this.headerHeight;
+		this.selectedRowIndex = -1;
+		while (e.localY >= searchY) {
+			this.selectedRowIndex++;
+			searchY += this.rowHeight;
+		}
+		if (this.selectedRowIndex > -1) {
+			this.selectedRowIndex += this.topRowIndex;
+		}
+		drawGrid(this.gridSprite.graphics);
 	}
 }
